73bb9f7ac426541bd8961c89694dbbff2aeb8753
Martin Heidegger <martin.heidegger@gmail.com>
2021-02-12 17:08:08 +0900

    adding binary that automatically sets the full icu

---
+++ node-full-icu.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+const { spawn } = require('child_process')
+const data = require('./full-icu')
+const env = data.icu_small ? {
+  ...process.env,
+  NODE_ICU_DATA: data.datPath()
+} : process.env
+
+spawn('/usr/bin/env', ['node', ...process.argv.slice(2)], { env, stdio: 'inherit' })
